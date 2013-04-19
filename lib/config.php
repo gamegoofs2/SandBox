20b81319b5ab1d21420e876a0338d5b3a9e2978c
Evan Uebel <rocketcoder@gmail.com>
2013-04-18 20:50:40 -0400

    Resuming development. Updated jQuery

---
+++ lib/config.php
@@ -0,0 +1,15 @@
+<?php
+//Change to the main user for the sandbox database.
+$dbuser = "sandbox";
+
+//Change to the password of the main database user.
+$dbpass = "password";
+
+//Change this to the name of the database to be used for sandbox chat.
+$dbname = "sandbox";
+
+//Change this to the address provide by your host for the MySQL database.
+//If an address was provided leave as localhost.
+$dbhost = "localhost";
+//After editting this file, run the install file located in /install
+?>
